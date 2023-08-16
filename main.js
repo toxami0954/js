35c00903d06106733566db56ca3e9977e0defe47
toxami0954 <142105740+toxami0954@users.noreply.github.com>
2023-08-16 21:05:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+ alert('Write your infarmation')
+ console.log('Write information people')
+ let value = prompt('Write your name')
+ console.log(prompt('Write your Surname'))
+ console.log(prompt('Write your living area'))
+ console.log(value)
+
+  console.log(prompt('Write your birth day'))
+  console.log(prompt('Write your age'))
+
+ let confirmation = confirm('Are you married')
+ console.log(confirmation)
+
+ console.log(prompt('Please give us suggestions to improve our site'))
+ alert('Thank your')
+console.log('😁😁😁😁')
